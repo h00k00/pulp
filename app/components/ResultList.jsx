1e8ce0e033425798201d2720fcd03e5afa430619
Harri Kaasalainen <harri.kaasalainen@gmail.com>
2016-08-16 12:11:32 +0300

    initial grid layout

--- app/components/ResultList.jsx
+++ app/components/ResultList.jsx
@@ -9,32 +9,28 @@ var AuthorWrapper = React.createClass({
     }
 });
 
-var BookRow = React.createClass({
+var BookItem = React.createClass({
     render: function() {
         var imageSrc = 'https://archive.org/services/img/' + this.props.records.identifier;
         var pdfUrl = 'https://archive.org/download/' + this.props.records.identifier + '/' + this.props.records.identifier + '.pdf';
         var itemUrl = 'https://archive.org/details/' + this.props.records.identifier;
         return (
-          <tr>
-                <td>
-                    <a target="_blank" href={pdfUrl}>
-                        <img src={imageSrc} className="cover"/>
-                    </a>
-                </td>
-                <td className="book-title">
+            <li>
+                <a target="_blank" href={pdfUrl}>
+                    <img src={imageSrc} className="cover"/>
+                </a>
+                <p>
                     <a target="_blank" href={itemUrl}>
                         {this.props.records.title}
                     </a>
-                </td>
-                <td className="book-author">
                     {this.props.records.creator}
-                </td>
-          </tr>
+                </p>
+            </li>
         );
   }
 });
 
-var BookTable = React.createClass({
+var BookGrid = React.createClass({
     // getInitialState: function () {
     // },
     // componentWillReceiveProps: function(nextProps) {
@@ -42,38 +38,31 @@ var BookTable = React.createClass({
     // componentDidMount: function () {
     // },
     render: function() {
-        var rows = [];
+        var itemList = [];
         var lastCategory = null;
-
+    
         function checkCategory(category, value) {
             if (value.subjects != undefined) {
                 return value.subjects.indexOf(category) != -1
-            } else
-            {
+            }
+            else {
                 return false;
             }
         }
     
         var that = this;
         this.props.records.forEach(function(records) {
-            rows.push(<BookRow records={records} key={records.identifier} />);
+            itemList.push(<BookItem records = {records} key = {records.identifier}/>);
         });
+    
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th><h3>Nimike</h3></th>
-                        <th><h3>Tekijä</h3></th>
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </table>
+            <ul className="grid">
+                {itemList}
+            </ul>
         );
     }
 });
-
-
+    
 var ResultList = React.createClass({
     getInitialState: function () {
         return {
@@ -86,11 +75,11 @@ var ResultList = React.createClass({
     render: function() {
         return (
             <div id="results">
-                <BookTable
-                    records = {this.props.records}/>
+                <BookGrid records = {this.props.records}/>
             </div>
         );
     }
 });
+
 
 module.exports = ResultList;
